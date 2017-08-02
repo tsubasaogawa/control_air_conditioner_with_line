6b3d79c6ee92d604e15685d777ff88c3b07fdcfc
Tsubasa Ogawa <hurikake@gmail.com>
2017-08-02 14:25:18 +0000

    add sample

---
+++ publisher.php
@@ -0,0 +1,8 @@
+<?php
+require("../phpMQTT.php");
+
+$mqtt = new phpMQTT("example.com", 1883, "phpMQTT Pub Example"); //Change client name to something unique
+if ($mqtt->connect()) {
+    $mqtt->publish("bluerhinos/phpMQTT/examples/publishtest","Hello World! at ".date("r"),0);
+    $mqtt->close();
+}
