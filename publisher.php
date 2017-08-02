d1d5094987c90fce32972ad4e31b5316c91c114a
Tsubasa Ogawa <hurikake@gmail.com>
2017-08-02 14:37:45 +0000

    add client user and pass

--- publisher.php
+++ publisher.php
@@ -3,8 +3,8 @@
 require('./phpMQTT/phpMQTT.php');
 require('./init.php');
 
-$mqtt = new phpMQTT($broker['host'], $broker['port'], 'publisher'); //Change client name to something unique
-if ($mqtt->connect()) {
+$mqtt = new phpMQTT($broker['host'], $broker['port'], 'publisher');
+if($mqtt->connect(true, null, $client['user'], $client['pass'])) {
     $mqtt->publish('ogawatest', 'remocon!', 0);
     $mqtt->close();
 }
